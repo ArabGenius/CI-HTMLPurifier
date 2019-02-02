d74f901fb0bb866649c4da61011491055d2c8829
AHMED AL-MASKARI <ahmed.maskari@gmail.com>
2019-02-02 16:07:50 +0400

    update my function to made an array for config so you anybody can make his config and Properties

--- MY_Security.php
+++ MY_Security.php
@@ -20,10 +20,29 @@
 
 class MY_Security extends CI_Security {
 
-	public function htmlpurify($html,$conf = null) {
+	public function htmlpurify($html,$conf = array()) {
 	    $getHTMLPurifier_folder = glob(APPPATH . 'third_party/htmlpurifier-*-standalone',GLOB_ONLYDIR);
 	    if(count($getHTMLPurifier_folder) > 0){
 		    include_once $getHTMLPurifier_folder[0].'/HTMLPurifier.standalone.php';
+		    if(count($conf) > 0){
+		    	$conf = HTMLPurifier_Config::createDefault();
+		    	if(isset($conf['HTML.AllowedElements']))$conf->set('HTML.AllowedElements', $conf['HTML.AllowedElements']);
+		    	if(isset($conf['HTML.AllowedAttributes']))$conf->set('HTML.AllowedAttributes', $conf['HTML.AllowedAttributes']);
+		    	if(isset($conf['CSS.AllowedProperties']))$conf->set('CSS.AllowedProperties', $conf['CSS.AllowedProperties']);
+		    	if(isset($conf['HTML.MaxImgLength']))$conf->set('HTML.MaxImgLength', $conf['HTML.MaxImgLength']);
+		    	if(isset($conf['CSS.MaxImgLength']))$conf->set('CSS.MaxImgLength', $conf['CSS.MaxImgLength']);
+		    	if(isset($conf['AutoFormat.AutoParagraph']))$conf->set('AutoFormat.AutoParagraph', $conf['AutoFormat.AutoParagraph']);
+		    }else{
+		    $conf = HTMLPurifier_Config::createDefault();
+		    //$conf->set('HTML.AllowedElements', 'p, a, ul, ol, li, h1, h2, h3, h4, h5, h6, br, strong, em, b, i');
+		    $conf->set('HTML.AllowedAttributes', 'src,style,href,height,width,title,alt');
+		    $conf->set('CSS.AllowedProperties','color,font-size,float,height,width,font-weight,font-style,font-family,text-align');
+		    $conf->set('HTML.MaxImgLength', '800');
+		    $conf->set('CSS.MaxImgLength', '800');
+		    $conf->set('AutoFormat.RemoveEmpty', true);
+		    $conf->set('AutoFormat.AutoParagraph', true);
+		    }
+
 		    $HTMLPurifier = new HTMLPurifier;
 		    $html = $HTMLPurifier->purify($html,$conf);
 		    return $html;
@@ -31,4 +50,11 @@ class MY_Security extends CI_Security {
 	    	die("You need to download HTMLPurifier.standalone and put it in the third_party folder");
 	    }
 	}
+
+
+	public function purify($html,$conf = array()) {
+	   return $this->htmlpurify($html,$conf);
+	}
+
+	
 }
